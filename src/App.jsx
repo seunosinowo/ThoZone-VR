d2f12828c0b33385de0e062e8bc731becf59d2e8
seunosinowo <oluwaseunpaul98@gmail.com>
2024-11-02 22:02:37 +0100

    Updated packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <NavBar />
     </div>
   )
 }
