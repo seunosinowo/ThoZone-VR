829f2f868ec7a1f9159ad28c0e81bc546c05bf65
seunosinowo <oluwaseunpaul98@gmail.com>
2024-11-05 13:15:55 +0100

    Implemented the Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import NavBar from './components/NavBar'
-
+import HeroSection from './components/HeroSection'
 
 const App = () => {
   return (
     <div>
-      <NavBar />
+      <>
+      <NavBar/>
+
+      <div className='max-w-7xl mx-auto pt-20 px-6'>
+        <HeroSection/>
+      </div>
+      
+      </>
     </div>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,7 +11,7 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="sticky top-20 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80"> 
+    <nav className="sticky top-1 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80"> 
       <div className="container px-4 mx-auto relative text-sm">
         <div className="flex justify-between items-center">
           <div className="flex items-center flex-shrink-0">
