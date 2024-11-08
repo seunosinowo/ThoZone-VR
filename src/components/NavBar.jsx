c9d7d4e4977c88869b37d1e0c9df48c045793cdc
seunosinowo <oluwaseunpaul98@gmail.com>
2024-11-08 15:42:59 +0100

    dd

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,7 +16,7 @@ const NavBar = () => {
         <div className="flex justify-between items-center">
           <div className="flex items-center flex-shrink-0">
               <img className="h-10 w-auto mr-2" src={logo} alt="logo" />
-              <span className="text-xl -tracking-tight">ThoZoneVR</span>
+              <span className="text-xl -tracking-tight">ThouytZoneVR</span>
           </div>
 
           <ul className="hidden lg:flex ml-14 space-x-14">
